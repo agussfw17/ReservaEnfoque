b2a178ee97586e7f090dd4bb8eaf08e21f8209b1
agussfw17 <agusfw17@gmail.com>
2026-03-09 13:11:08 -0300

    FARTO

--- middlewares/login.js
+++ middlewares/login.js
@@ -1,34 +1,34 @@
-import { YO, PABLO_BOLLA } from '../utils/exportUsers.js';
+import { YO, PABLO_BOLLA } from "../utils/exportUsers.js";
 
 export async function login(user) {
-    const URL_LOGIN = process.env.URL_LOGIN;
-    
-    const payload = {
-        user: user.email,
-        password: user.password
-    };
-    try {
-        const res = await fetch(URL_LOGIN, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(payload)
-        });
+  const URL_LOGIN = process.env.URL_LOGIN;
 
-        if (!res.ok) {
-            throw new Error(`HTTP error! status: ${res.status}`);
-        }
+  const payload = {
+    user: user.email,
+    password: user.password,
+  };
+  try {
+    const res = await fetch(URL_LOGIN, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    });
 
-        const data = await res.json();
-
-        const token = data.token;
-        const id = data.description.id;
-
-        return {id, token};
-    } catch (err) {
-        console.error('Error al hacer signin:', err.message);
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: ${res.status}`);
     }
+
+    const data = await res.json();
+
+    const token = data.token;
+    const id = data.description.id;
+
+    return { id, token };
+  } catch (err) {
+    console.error("Error al hacer signin:", err.message);
+  }
 }
 
-login(PABLO_BOLLA);
+login(FARTO);
--- utils/exportUsers.js
+++ utils/exportUsers.js
@@ -1,14 +1,15 @@
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const usersPath = path.join(__dirname, 'users.json');
-const rawData = fs.readFileSync(usersPath, 'utf-8');
+const usersPath = path.join(__dirname, "users.json");
+const rawData = fs.readFileSync(usersPath, "utf-8");
 
 export const users = JSON.parse(rawData);
 
 export const YO = users.YO;
 export const PABLO_BOLLA = users.PABLO_BOLLA;
+export const FARTO = users.FARTO;
