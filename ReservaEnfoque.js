23289f54597675cbd0acc0a7ee73f8031416ac83
agussfw17 <agusfw17@gmail.com>
2026-03-23 16:35:20 -0300

    export delfi

--- ReservaEnfoque.js
+++ ReservaEnfoque.js
@@ -2,7 +2,7 @@ import { login } from "./middlewares/login.js";
 import { activityCategory } from "./middlewares/activityCategory.js";
 import { activityTime } from "./middlewares/activityTime.js";
 
-import { YO, PABLO_BOLLA, FARTO, LODEIRO } from "./utils/exportUsers.js";
+import { YO, PABLO_BOLLA, FARTO, LODEIRO, DELFI } from "./utils/exportUsers.js";
 
 import dotenv from "dotenv";
 dotenv.config();
